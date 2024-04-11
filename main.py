363d42076220a9d9217e007b05ee6043bc79e093
stefan <secrieru23022@gmail.com>
2024-04-11 11:15:24 +0300

    Sync

--- main.py
+++ main.py
@@ -21,6 +21,50 @@ def quarter_round(a, b, c, d):
     b = (b << 7 | b >> 25) & 0xFFFFFFFF
     return a, b, c, d
 
-# Example usage:
-a, b, c, d = quarter_round(0x11111111, 0x01020304, 0x9b8d6f43, 0x01234567)
-print(hex(a), hex(b), hex(c), hex(d))
+
+def QUARTERROUND(a, b, c, d, chacha_state):
+    new_a, new_b, new_c, new_d = quarter_round(chacha_state[a], chacha_state[b], chacha_state[c], chacha_state[d])
+    chacha_state[a] = new_a
+    chacha_state[b] = new_b
+    chacha_state[c] = new_c
+    chacha_state[d] = new_d
+    return chacha_state
+
+# a, b, c, d = quarter_round(0x11111111, 0x01020304, 0x9b8d6f43, 0x01234567)
+# print(hex(a), hex(b), hex(c), hex(d))
+
+def print_chacha_state(chacha_state):
+    for i in chacha_state:
+        print(hex(i))
+
+
+chacha_state = [0x879531e0, 0xc5ecf37d, 0x516461b1, 0xc9a62f8a,
+                0x44c20ef3, 0x3390af7f, 0xd9fc690b, 0x2a5f714c,
+                0x53372767, 0xb00a5631, 0x974c541a, 0x359e9963,
+                0x5c971061, 0x3d631689, 0x2098d9d6, 0x91dbd320]
+
+QUARTERROUND(2, 7, 8, 13, chacha_state)
+print_chacha_state(chacha_state)
+
+
+# Initial chacha state
+# cccccccc  cccccccc  cccccccc  cccccccc
+# kkkkkkkk  kkkkkkkk  kkkkkkkk  kkkkkkkk
+# kkkkkkkk  kkkkkkkk  kkkkkkkk  kkkkkkkk
+# bbbbbbbb  nnnnnnnn  nnnnnnnn  nnnnnnnn
+# c=constant k=key b=blockcount n=nonce
+
+
+# For one round 
+# Column rounds
+QUARTERROUND(0, 4, 8, 12, chacha_state)
+QUARTERROUND(1, 5, 9, 13, chacha_state)
+QUARTERROUND(2, 6, 10, 14, chacha_state)
+QUARTERROUND(3, 7, 11, 15, chacha_state)
+# Diagonal rounds
+QUARTERROUND(0, 5, 10, 15, chacha_state)
+QUARTERROUND(1, 6, 11, 12, chacha_state)
+QUARTERROUND(2, 7, 8, 13, chacha_state)
+QUARTERROUND(3, 4, 9, 14, chacha_state)
+
+print_chacha_state(chacha_state)
